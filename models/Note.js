12eb647e2451b08e44bc7d7747ec0e35feefcd34
Winfield H <winterfreshness@gmail.com>
2019-01-12 11:30:37 -0500

    Made a new Note.js model

---
+++ models/Note.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+// Reference to the Schema constructor
+let Schema = mongoose.Schema;
+
+// Make a schema for the note model
+let noteSchema = new Schema({
+    // Id referencing the id's from the Headline model
+    _headlineId: {
+      type: Schema.Types.ObjectId,
+      ref: "Headline"
+    },
+    // The date created
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    // The text of the note
+    noteText: {
+      type: String
+    }
+});
+
+// Make the model using the schema
+let Note = mongoose.model("Note", noteSchema);
+
+// Export the module to use in the app
+module.exports = Note;
